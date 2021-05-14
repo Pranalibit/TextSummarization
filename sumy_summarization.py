0f2dd055782def5a3d2842760ebe2320b9ff60d2
Pranali <55937286+Pranalibit@users.noreply.github.com>
2021-05-14 17:03:10 +0530

    Text Summarizer + Flask App Added

---
+++ sumy_summarization.py
@@ -0,0 +1,13 @@
+# Sumy Packages
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lex_rank import LexRankSummarizer
+
+
+def sumy_summary(docx):
+    parser = PlaintextParser.from_string(docx, Tokenizer("english"))
+    lex_summarizer = LexRankSummarizer()
+    summary = lex_summarizer(parser.document, 3)
+    summary_list = [str(sentence) for sentence in summary]
+    result = ' '.join(summary_list)
+    return result
